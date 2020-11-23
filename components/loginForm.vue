4175e28e3d65a49796c033e7e62017db96316d9c
阳姐 <xzllbfs@163.com>
2020-11-23 14:57:54 +0800

    feat: init

---
+++ components/loginForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <fieldset v-if="!isLogin" class="form-group">
+      <input v-model="user.username" class="form-control form-control-lg" type="text" placeholder="用户名" required>
+    </fieldset>
+    <fieldset class="form-group">
+      <input v-model="user.email" class="form-control form-control-lg" type="email" placeholder="邮箱" required>
+    </fieldset>
+    <fieldset class="form-group">
+      <input v-model="user.password" class="form-control form-control-lg" type="password" placeholder="密码" required minlength="8">
+    </fieldset>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    isLogin () {
+      return this.$route.name === 'login'
+    }
+  },
+  data () {
+    return {
+      user: {
+        username: '',
+        email: 'lpzmail@163.com',
+        password: '12345678'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/register/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">注册</h1>
+          <p class="text-xs-center">
+            <nuxt-link to="/login">已有账号？去登录</nuxt-link>
+          </p>
+
+          <ul class="error-messages">
+            <template v-for="(messages, field) in errors">
+              <li
+                v-for="(message, index) in messages"
+                :key="index"
+              >{{ field }} {{ message }}</li>
+            </template>
+          </ul>
+          <login-form ref="loginForm"/>
+          <button
+            @click="onSubmit"
+            class="btn btn-lg btn-primary pull-xs-right">
+            注册
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { login, register } from '@/api/user'
+import loginForm from '../../components/loginForm.vue'
+
+// 仅在客户端加载 js-cookie 包
+const Cookie = process.client ? require('js-cookie') : undefined
+
+export default {
+  components: { loginForm },
+  middleware: 'notAuthenticated',
+  name: 'RegisterIndex',
+  data () {
+    return {
+      user: {
+        username: '',
+        email: 'lpzmail@163.com',
+        password: '12345678'
+      },
+      errors: {} // 错误信息
+    }
+  },
+
+  methods: {
+    async onSubmit () {
+      const user = this.$refs.loginForm.user
+      console.log('注册信息', user)
+      try {
+        // 提交表单请求登录
+        const { data } = await register({ user })
+
+        console.log(data)
+        // TODO: 保存用户的登录状态
+        this.$store.commit('setUser', data.user)
+
+        // 为了防止刷新页面数据丢失，我们需要把数据持久化
+        Cookie.set('user', data.user)
+
+        // 跳转到首页
+        this.$router.push('/')
+      } catch (err) {
+        // console.log('请求失败', err)
+        this.errors = err.response.data.errors
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
